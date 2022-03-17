a7d8c0e1db221a0266095b55ea5008c56bad6dda
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-17 11:31:01 +0600

    normal css add

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
     const {name, capital, population, region, timezones} = props.country;
     console.log(props.country);
     return (
-        <div>
+        <div className='country'>
             <h1>Name: {name}</h1>
             <p>Capital: {capital}</p>
             <p>Population: {population}</p>
