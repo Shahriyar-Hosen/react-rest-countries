637fde49dfc6890fd68b727eee704371b3bc2a9c
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-17 12:35:28 +0600

    Name remove

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -5,7 +5,7 @@ const Country = (props) => {
     const {name, capital, population, region, timezones, flags} = props.country;
     return (
         <div className='country'>
-            <h1>Name: {name}</h1>
+            <h1>{name}</h1>
             <img src={flags.png} alt="" />
             <p>Capital: {capital}</p>
             <p>Population: {population}</p>
