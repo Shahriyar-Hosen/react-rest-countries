bf62b9ccf832d47e8c6701f26b857ce07e71a5fa
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-17 10:52:07 +0600

    React rest country

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+// const Countries = () => {
+//   const [countries, setCountries] = useState([]);
+//     useEffect(() => {
+//       fetch("https://restcountries.com/v2/all")
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//     },[])
+//   return (
+//     <div>
+//       <h1>Visiting every country in tha world</h1>
+//       <p>{countries.length}</p>
+//       {
+//         countries.map((country) =>  <Country country = {country}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// const Country = (props) => {
+//   console.log(props.country);
+//   return (
+//     <div>
+//       <h2>Name: {props.country.name}</h2>
+//       <p>Capital: {props.country.capital}</p>
+//     </div>
+//   )
+// }
+
+export default App;
